be667d72f6588530b5223aa8028031f39cfad7d5
sriramh14 <sriramhari1014@gmail.com>
2026-06-18 16:51:29 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 """Models for RGB-to-HSI reconstruction."""
 
-from .dps_rgb2hsi_model import DPSRGB2HSI, ModelConfig, LinearHSIToRGBOperator
+from .dps_rgb2hsi_model import DPSRGB2HSI, ModelConfig
 
-__all__ = ["DPSRGB2HSI", "ModelConfig", "LinearHSIToRGBOperator"]
+__all__ = ["DPSRGB2HSI", "ModelConfig"]
